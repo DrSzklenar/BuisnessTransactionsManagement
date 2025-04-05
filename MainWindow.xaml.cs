63db38d1557c8d669092d36ae8e19daee6f82abc
DrSzklenar <112695544+DrSzklenar@users.noreply.github.com>
2025-04-05 15:36:48 +0200

    added pages

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,36 @@ namespace BuisnessTransactionsManagement
     /// </summary>
     public partial class MainWindow : Window
     {
+        public SearchPage s1 = new SearchPage();
+        public NewItemPage n1 = new NewItemPage();
+        public ModifyPage m1 = new ModifyPage();
+        public Home home = new Home();
+        
         public MainWindow()
         {
             InitializeComponent();
+            Main.Content = home;
+        }
+
+        private void miSearchClick(object sender, RoutedEventArgs e)
+        {
+            Main.Content = s1;
+            
+        }
+
+        private void miNewClick(object sender, RoutedEventArgs e)
+        {
+            Main.Content = n1;
+        }
+
+        private void miModifyPageClick(object sender, RoutedEventArgs e)
+        {
+            Main.Content = m1;
+        }
+
+        private void miHomeClick(object sender, RoutedEventArgs e)
+        {
+            Main.Content = home;
         }
     }
 }
